e3f96e319696b7cea3e9b358ac03f3dff88f5ebb
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-02-04 13:28:24 +0530

    aiml

--- aiml/q23.py
+++ aiml/q23.py
@@ -2,9 +2,8 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 df=pd.read_csv("tips.csv")
-plt.plot(df.groupby('day')['tip'].mean(), marker='o')
-plt.title("Average Tip by Day")
+plt.bar(df.groupby('day')['tip'].mean().index, df.groupby('day')['tip'].mean().values , color='green', edgecolor='black')
+plt.title("Average Tip by Day")     
 plt.xlabel("Day")
 plt.ylabel("Average Tip")
-plt.grid()
 plt.show()
