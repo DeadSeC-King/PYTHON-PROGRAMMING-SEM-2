4d96a9535316ff5d4bae99ed3bdd4532f36349dd
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-02-07 22:14:24 +0530

    Remove unnecessary blank lines in 3e10.py

--- experiment-03-python/3e10.py
+++ experiment-03-python/3e10.py
@@ -1,12 +1,13 @@
 n = 5
-
 for i in range(n):
+    # L
     for j in range(1, n - i + 1):
         print(j, end="")
+    #m
     if i > 0:
-        for k in range(i):
-            print("* ", end="")
-    for j in range(n - i + 1, 0, -1):
+        for k in range(2 * i - 1):
+            print("*", end="")
+    #r
+    for j in range(n - i, 0, -1):
         print(j, end="")
-
     print()
