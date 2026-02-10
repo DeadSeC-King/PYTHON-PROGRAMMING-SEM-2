a10bcae0933f2b0e4477718595462f19c2621699
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-02-10 11:18:52 +0530

    Refactor number and star printing logic in 3e10.py for clarity

--- experiment-03-python/3e10.py
+++ experiment-03-python/3e10.py
@@ -1,13 +1,17 @@
 n = 5
-for i in range(n):
-    # L
-    for j in range(1, n - i + 1):
+
+for i in range(n, 0, -1):
+
+    # Print numbers from 1 to i
+    for j in range(1, i + 1):
         print(j, end="")
-    #m
-    if i > 0:
-        for k in range(2 * i - 1):
-            print("*", end="")
-    #r
-    for j in range(n - i, 0, -1):
+
+    # Print stars
+    stars = n - i
+    print("*" * (1* stars), end="")
+
+    # Print numbers from i to 1
+    for j in range(i, 0, -1):
         print(j, end="")
-    print()
+
+    print()  # New line
