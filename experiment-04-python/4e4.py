3af1746d96ae0b5e0f3e0636f40c3b15e6e61f03
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-02-17 10:21:55 +0530

    done

--- experiment-04-python/4e4.py
+++ experiment-04-python/4e4.py
@@ -1,5 +1,12 @@
-#wap to input a string and count a substring repeatedly in the string
 s = input("Enter a string: ")
 sub = input("Enter a substring to count: ")
-count = s.count(sub)
-print("The substring '{}' appears {} times in the string.".format(sub, count))
+
+count = 0
+i = 0
+
+while i <= len(s) - len(sub):
+    if s[i:i+len(sub)] == sub:
+        count += 1
+    i += 1
+
+print("Count =", count)
